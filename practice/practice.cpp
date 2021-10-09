bf7b5e673fea5f151e3c5d705dc253902d60a56e
AMRN0 <ali.noorani@mail.bcu.ac.uk>
2021-10-09 20:16:47 +0100

    day 1 hello world setup

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <string>
+#include "practice.h" //importing from an internal file which contains declrations and <iostream>.
 
-int main() //main function
+int main() //main function. int in this case is the return type.
 {
-    std::string hi = "hello\n"; //declaring a string variable called "hi" with the value of "hello".
-    std::cout << hi << std::endl;
+    hi = "hello\n"; //defining a string variable called "hi" with the value of "hello".
+    //std above is a namespace allows to group code and avoid naming conflicts. same var name, but different namespaces.
+    std::cout << hi << std::endl; //outputting a variable.
+    return 0; //telling the program that everything went right. the "0" is an int. related to the return type.
 }
--- practice/practice.h
+++ practice/practice.h
@@ -1,1 +1,4 @@
 #pragma once
+#include <iostream> //importing a library external to the main program folder. use "" to import an internal one. input output stream.
+
+std::string hi; //declaring a variable called "hi".
