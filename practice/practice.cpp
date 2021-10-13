62e6c1d9373a94b1b3984e805fed7bfd6f3f1e61
AMRN0 <ali.noorani@mail.bcu.ac.uk>
2021-10-13 13:15:53 +0100

    functions and variables

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,9 +1,56 @@
 #include "practice.h" //importing from an internal file which contains declrations and <iostream>.
 
-int main() //main function. int in this case is the return type.
+int main() //main function. int in this case is the return type. this function is called when the program is executed.
 {
     hi = "hello\n"; //defining a string variable called "hi" with the value of "hello".
+
+    sup = hi; //setting sup to the same value as hi but it doesnt point to it.
+
+    hi = "yo"; //so i can change hi but it wouldnt change sup.
+
+    number = 1;
     //std above is a namespace allows to group code and avoid naming conflicts. same var name, but different namespaces.
-    std::cout << hi << std::endl; //outputting a variable.
+
+
+    std::cout << hi << " and sup is " << sup << std::endl; //outputting a variable on the console.
+
+    printf("%i\n", number); //comes from C language and is used to print out variables, but std in c++ is better.
+
+
+    //getting user input
+    std::cout << "how many slices of pizza do you have?\n" << std::endl;
+    std::cin >> slices ;
+    std::cout << "you have " << slices << " of pizza." << std::endl;
+    
+
+    //creating pre made functions.
+    std::cout << "what is the base" << std::endl; //printing.
+    std::cin >> base; //getting the user input for base.
+    
+    std::cout << "what is the exponent" << std::endl; //printing exponent.
+    std::cin >> exponent; //asking for exponent
+
+    myPower = pow(base, exponent); //assigning myPower to calculate power with arguments base and exponent.
+    std::cout << myPower << std::endl;
+
+
+    //calling the custom power function.
+    std::cout << "what is x?";
+    std::cin >> x;
+    std::cout << "What is y?";
+    std::cin >> y;
+    POWER = power(x, y);
+    std::cout << POWER << std::endl;
     return 0; //telling the program that everything went right. the "0" is an int. related to the return type.
+}
+
+//creating custom functions. useful for calling code that you will use multiple times.
+double power(double x, int y)
+{
+    result = 1;
+    for (i = 0; i < y; i++)
+    {
+        result *= x;
+    }
+    return result;
 }
--- practice/practice.h
+++ practice/practice.h
@@ -1,4 +1,20 @@
 #pragma once
 #include <iostream> //importing a library external to the main program folder. use "" to import an internal one. input output stream.
+#include <cmath> //including mathematical functions into c++ like the power function.
 
 std::string hi; //declaring a variable called "hi".
+int slices;
+int SLICES; //not the same as slices as c++ is case sensitive.
+int number;
+std::string sup;
+
+//declaring a function.
+double power(double,int);
+double POWER;
+double x;
+int y;
+double result;
+int i;
+
+int base, exponent; //creating variables to store the base and exponent of the power function.
+double myPower;
