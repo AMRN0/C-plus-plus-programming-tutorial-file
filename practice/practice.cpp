d8dd0bd14fe20dcce49c3ee517cd2024c17e1667
AMRN0 <ali.noorani@mail.bcu.ac.uk>
2021-10-13 16:07:43 +0100

    data types commit

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -2,6 +2,59 @@
 
 int main() //main function. int in this case is the return type. this function is called when the program is executed.
 {
+    //all text has been moved to void print_pow().
+
+
+
+    /*data types:
+    int - integer
+    char - character
+    float - floating
+    double - decimals
+    bool - true or false
+    strings - sequences of characters
+    */
+
+    /*int data types:
+    short
+    int
+    long
+    long long
+    unsigned short
+    unsigned int
+    unsigned long
+    unsigned long long*/
+
+    /*char data type has single quotes: ''.
+    double quotes: "" are reserved for string type.
+    can store ascii numbers of the characters.
+    x = 129
+    std::cout << x << std::endl; this would output the character stored in the variable x*/
+
+
+    bruh = 99999999;
+    std::cout << bruh << std::endl;
+    print_pow(a, b);
+
+    return 0; //telling the program that everything went right. the "0" is an int. related to the return type.
+}
+
+
+//creating custom functions. useful for calling code that you will use multiple times.
+double power(double x, int y) //currently x and y are parameters.
+{
+    result = 1;
+    for (i = 0; i < y; i++)
+    {
+        result *= x;
+    }
+    return result;
+}
+
+
+//creating void functions.
+void print_pow(double a, int b)
+{
     hi = "hello\n"; //defining a string variable called "hi" with the value of "hello".
 
     sup = hi; //setting sup to the same value as hi but it doesnt point to it.
@@ -19,14 +72,14 @@ int main() //main function. int in this case is the return type. this function i
 
     //getting user input
     std::cout << "how many slices of pizza do you have?\n" << std::endl;
-    std::cin >> slices ;
+    std::cin >> slices;
     std::cout << "you have " << slices << " of pizza." << std::endl;
-    
+
 
     //creating pre made functions.
     std::cout << "what is the base" << std::endl; //printing.
     std::cin >> base; //getting the user input for base.
-    
+
     std::cout << "what is the exponent" << std::endl; //printing exponent.
     std::cin >> exponent; //asking for exponent
 
@@ -39,18 +92,15 @@ int main() //main function. int in this case is the return type. this function i
     std::cin >> x;
     std::cout << "What is y?";
     std::cin >> y;
-    POWER = power(x, y);
+    POWER = power(x, y); //currently x and y are arguments. this x and y can be differently named to the parameters.
     std::cout << POWER << std::endl;
-    return 0; //telling the program that everything went right. the "0" is an int. related to the return type.
-}
 
-//creating custom functions. useful for calling code that you will use multiple times.
-double power(double x, int y)
-{
-    result = 1;
-    for (i = 0; i < y; i++)
-    {
-        result *= x;
-    }
-    return result;
+
+    std::cout << "what is a?";
+    std::cin >> a;
+    std::cout << "what is b?";
+    std::cin >> b;
+    std::cout << pow(a, b) << std::endl;
+    std::cout << power(a, b) << std::endl;
+
 }
--- practice/practice.h
+++ practice/practice.h
@@ -18,3 +18,12 @@ int i;
 
 int base, exponent; //creating variables to store the base and exponent of the power function.
 double myPower;
+
+
+//creating void functions.
+void print_pow(double, int);
+double a;
+int b;
+
+//main function declarations.
+long long int bruh; //here i am using a double long integer. i cannot use a double long double as long is an int type.
